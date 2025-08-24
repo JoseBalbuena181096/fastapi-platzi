73d4560cd8a2e035997fca8ff906223b67c1992f
José Balbuena <45837292+JoseBalbuena181096@users.noreply.github.com>
2025-08-24 13:16:29 -0600

    Relaciones en Fast Api y SQLModel

--- curso-fastapi-project/app/routers/transaction.py
+++ curso-fastapi-project/app/routers/transaction.py
@@ -1,10 +1,24 @@
 from fastapi import APIRouter, HTTPException, status
 from sqlmodel import select
-from models import Transaction
+from models import Transaction, TransactionCreate, Customer
 from db import SessionDep
  
 router = APIRouter()
 
-@router.post("/transaction", tags=["transactions"])
-async def create_transaction(transaction_data: Transaction):
+@router.post("/transaction", status_code=status.HTTP_201_CREATED, tags=["transactions"])
+async def create_transaction(transaction_data: TransactionCreate, session: SessionDep):
+    transaction_data_dict = transaction_data.model_dump()
+    customer = session.get(Customer, transaction_data_dict.get("customer_id"))
+    if not customer:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Customer not found")
+    transaction_db = Transaction.model_validate(transaction_data_dict)
+    session.add(transaction_db)
+    session.commit()
+    session.refresh(transaction_db)
+    return transaction_db
+
+@router.get("/transaction", tags=["transactions"])
+async def get_transactions(session: SessionDep):
+    query = select(Transaction)
+    transaction_data = session.exec(query).all()
     return transaction_data
